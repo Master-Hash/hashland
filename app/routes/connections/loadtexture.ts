3b5c78188c1448e9b85e3fd5e9167ed011489b99
Master-Hash <hash@hash.memorial>
2025-01-29 21:01:59 +0100

    fix: 纹理现在只会定义一次

--- app/routes/connections/loadtexture.ts
+++ app/routes/connections/loadtexture.ts
@@ -3,32 +3,34 @@ import { Assets } from "pixi.js";
 import chronicles from "./chronicle.js";
 
 export async function loadTexture() {
-  Assets.add({
-    src: "/zodiac-white.png",
-    alias: "zodiac",
-  });
-  chronicles.forEach((chronicle) => {
-    if (chronicle.emoji.match(/[\p{RGI_Emoji}\u26f0]/v)) {
-      const codePoint = [...chronicle.emoji]
-        .map((char) => char.codePointAt(0)?.toString(16))
-        .join("-");
-      // console.log(codePoint);
-      const emojiName = chars[codePoint];
-      Assets.add({
-        src:
-          "/fluent-emoji-high-contrast/fluent-emoji-high-contrast_" +
-          emojiName +
-          ".png",
-        alias: chronicle.emoji,
-      });
-    } else {
-      const [iconSet, iconName] = chronicle.emoji.split(":");
-      Assets.add({
-        src: "/" + iconSet + "/" + iconSet + "_" + iconName + ".png",
-        alias: chronicle.emoji,
-      });
-    }
-  });
+  if (Assets.resolver.resolve("zodiac").src !== "/zodiac-white.png") {
+    Assets.add({
+      src: "/zodiac-white.png",
+      alias: "zodiac",
+    });
+    chronicles.forEach((chronicle) => {
+      if (chronicle.emoji.match(/[\p{RGI_Emoji}\u26f0]/v)) {
+        const codePoint = [...chronicle.emoji]
+          .map((char) => char.codePointAt(0)?.toString(16))
+          .join("-");
+        // console.log(codePoint);
+        const emojiName = chars[codePoint];
+        Assets.add({
+          src:
+            "/fluent-emoji-high-contrast/fluent-emoji-high-contrast_" +
+            emojiName +
+            ".png",
+          alias: chronicle.emoji,
+        });
+      } else {
+        const [iconSet, iconName] = chronicle.emoji.split(":");
+        Assets.add({
+          src: "/" + iconSet + "/" + iconSet + "_" + iconName + ".png",
+          alias: chronicle.emoji,
+        });
+      }
+    });
+  }
   return await Assets.load([
     "zodiac",
     ...chronicles.map((chronicle) => chronicle.emoji),
